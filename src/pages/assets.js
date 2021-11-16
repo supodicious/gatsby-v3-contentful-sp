32d69b792035de29cc00a1eb7929abe08f66db25
sp <supodicious@gmail.com>
2021-11-15 23:12:49 -0600

    Update assets.js

--- src/pages/assets.js
+++ src/pages/assets.js
@@ -2,31 +2,15 @@ import React from "react"
 import SEO from "../components/SEO"
 import Layout from "../components/Layout"
 import AllAssets from "../components/AllAssets"
-import { ThemeProvider } from "styled-components"
-
-const theme = {
-  colors: {
-    navbar: "#C9EDFF",
-    header: "#EBFCFF",
-    navlink: "#006699",
-    navlinkHover: "#47C2FF",
-    body: "#F7F7F7",
-    assetCard: '#D8F2FF',
-    footer: "#006699",
-  },
-  mobile: "768px",
-}
 
 const Assets = () => {
   return (
-    <ThemeProvider theme={theme}>
       <Layout>
         <SEO title="Assets " />
         <main className="page">
           <AllAssets />
         </main>
       </Layout>
-    </ThemeProvider>
   )
 }
 
